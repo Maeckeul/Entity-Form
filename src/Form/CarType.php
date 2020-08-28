dbfa5266f2a9802adfb079c66c3f00be073ba6b1
Maeckeul <cmaeckeul@gmail.com>
2020-08-28 16:22:27 +0200

    :construction: constraints on Entity Car

--- src/Form/CarType.php
+++ src/Form/CarType.php
@@ -6,14 +6,31 @@ use App\Entity\Car;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints;
 
 class CarType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('model')
-            ->add('year')
+            ->add('model', null, [
+                'constraints' => [
+                    new Constraints\NotBlank(),
+                    new Constraints\Length([
+                        'min' => 2,
+                        'max' => 255,
+                    ]),
+                ]
+            ])
+            ->add('year', null, [
+                'constraints' => [
+                    new Constraints\NotBlank(),
+                    new Constraints\Range([
+                        'min' => 1900,
+                        'max' => date('Y') + 1
+                    ]),
+                ]
+            ])
         ;
     }
 
